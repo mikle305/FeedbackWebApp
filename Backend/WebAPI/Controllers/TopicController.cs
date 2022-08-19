6101df161418bf6d91bcb9467a4f1fdd030a2c91
Mikhail <mikle3305@gmail.com>
2022-08-19 21:11:48 +0300

    Done. Get all topics

---
+++ Backend/WebAPI/Controllers/TopicController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.DB;
+
+namespace WebAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TopicController: ControllerBase
+{
+    private readonly ApplicationContext _db;
+
+    public TopicController(ApplicationContext db)
+    {
+        _db = db;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var topics = await _db.Topics.ToListAsync();
+        return Ok(new
+        {
+            Success = true,
+            Topics = topics
+        });
+    }
+}

b24c7a8a9a993fdbc28912e095deb84e88fd1574
Mikhail <mikle3305@gmail.com>
2022-08-20 22:18:48 +0300

    Change. Refactor, Tailwind import

--- Frontend/App/src/components/app/app.module.ts
+++ Frontend/App/src/components/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import {FeedbackComponent} from "../feedback/feedback.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeedbackComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]

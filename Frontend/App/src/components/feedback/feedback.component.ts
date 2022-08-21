b61400ddbe08861d30616790d575e4d3d13f7911
Mikhail <mikle3305@gmail.com>
2022-08-21 05:01:56 +0300

    Done. Сверстал окошко фидбэка

---
+++ Frontend/App/src/components/feedback/feedback.component.ts
@@ -0,0 +1,12 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'app-feedback',
+  templateUrl: './feedback.component.html',
+  styles: [
+    `.verdanaFont{font-size:14px; font-family:Verdana;}
+    .segoePrintFont{font-size:14px; font-family:"Segoe Print";}`
+  ]
+})
+export class FeedbackComponent {
+}
